b691b3c41afc9f71dbfc3a3444b667ab0bf53901
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-25 18:42:01 +0530

    chat compoent

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { user } from "../Join/Join";
+import socketIo from "socket.io-client";
+import "./Chat.css";
+import sendLogo from "../../images/send.png";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import closeIcon from "../../images/closeIcon.png";
+const ENDPOINT = "http://localhost:7000/";
+let socket;
 
 export const Chat = () => {
+   useEffect(() => {
+      socket = socketIo(ENDPOINT, { transports: ["websocket"] });
+      socket.on("connect", () => {
+         alert("Connected");
+      });
+   });
+
    return (
       <div>
          <h1>Chat</h1>
