5da4cd1229f69ff219f61f0dc88e726ffb384b09
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-25 19:19:52 +0530

    message sent success

--- server/server.js
+++ server/server.js
@@ -1,24 +1,51 @@
-const app = require("express")();
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, {
-   cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-   },
-});
+const http = require("http");
+const express = require("express");
 const cors = require("cors");
-app.use(cors());
+const socketIO = require("socket.io");
 
-const PORT = 7000 || process.env.PORT;
+const app = express();
+const port = process.env.PORT || 7000;
+
+const users = [{}];
+
+app.use(cors());
+app.get("/", (req, res) => {
+   res.send("HELL ITS WORKING");
+});
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
 
 io.on("connection", (socket) => {
-   console.log("socket connected at id " + socket.id);
+   console.log("New Connection");
+
+   socket.on("joined", ({ user }) => {
+      users[socket.id] = user;
+      console.log(`${user} has joined `);
+      socket.broadcast.emit("userJoined", {
+         user: "Admin",
+         message: ` ${users[socket.id]} has joined`,
+      });
+      socket.emit("welcome", {
+         user: "Admin",
+         message: `Welcome to the chat,${users[socket.id]} `,
+      });
+   });
+
+   socket.on("message", ({ message, id }) => {
+      io.emit("sendMessage", { user: users[id], message, id });
+   });
 
    socket.on("disconnect", () => {
-      console.log(`${socket.id} socket disconnected  `);
+      socket.broadcast.emit("leave", {
+         user: "Admin",
+         message: `${users[socket.id]}  has left`,
+      });
+      console.log(`user left`);
    });
 });
 
-httpServer.listen(PORT, () => {
-   console.log("server listining at port " + PORT);
+server.listen(port, () => {
+   console.log(`Working`);
 });
