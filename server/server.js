f40a81afb2ebd24ac76b18511f974da65305d6c5
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-25 16:55:30 +0530

    socket connection success

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const app = require("express")();
 const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+const io = require("socket.io")(httpServer, {
+   cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+   },
+});
 const PORT = 7000;
 io.on("connection", (socket) => {
-   console.log("socket connected" + socket.id);
+   console.log("socket connected at id " + socket.id);
 });
 
 httpServer.listen(PORT, () => {
